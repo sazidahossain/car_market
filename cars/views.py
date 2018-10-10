c553775272bcbf9b058dc754966e39e8c3c4ba7d
Sazida Hossain <sazidahossain@Sazidas-MacBook-Pro.local>
2018-10-10 16:13:48 +0300

    improvements needed

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Car
-
+from .forms import CarForm
+from django.contrib import messages
 def car_list(request):
 	cars = Car.objects.all()
 	context = {
@@ -12,21 +13,43 @@ def car_list(request):
 def car_detail(request, car_id):
 	car = Car.objects.get(id=car_id)
 	context = {
-		"car": car,
+		"cars": car,
 	}
 	return render(request, 'car_detail.html', context)
 
 
 def car_create(request):
-	#Complete Me
-	return render(...)
+	form=CarForm()
+	if request.method=="POST":
+		form=CarForm(request.POST,request.FILES or None)
+		if form.is_valid():
+			form.save()
+			messages.add_message(request, messages.INFO, 'Created.')
+			return redirect('car-list')
+			
+	context={
+		"form":form
+	}
+	return render(request,'create.html',context)
 
 
 def car_update(request, car_id):
-	#Complete Me
-	return render(...)
+	car_obj=Car.objects.get(id=car_id)
+	form=CarForm(instance=car_obj)
+	if request.method=="POST":
+		form=CarForm(request.POST,request.FILES or None,instance=car_obj)
+		if form.is_valid():
+			form.save()
+			messages.add_message(request, messages.INFO, 'Updated!')
+			return redirect('car-list')
+	context={
+		"car_obj":car_obj,
+		"form":form,
+	}
+	return render(request,'update.html',context)
 
 
 def car_delete(request, car_id):
-	#Complete Me
-	return render(...)
+	car_obj=Car.objects.get(id=car_id).delete()
+	messages.add_message(request, messages.INFO, 'Deleted!')
+	return redirect('car-list')
